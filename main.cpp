4bcace56dbf3678b7cc27f938afe7a5a07bbb2e2
Arthur Henrique <39110264+thurcst@users.noreply.github.com>
2019-05-09 23:45:48 -0300

    Main 70% Concluida / Extensões quase completas
    Falta tratar as expressões e checar as respostas.

--- main.cpp
+++ main.cpp
@@ -33,12 +33,6 @@ vector <Node*> applyRule(Node* node) {
     return insertedNodes;
 }
 
-void checkContradictions(vector<Node*> insertedNodes) {
-    for (int i = 0; i < insertedNodes.size(); i++) {
-        if(insertedNodes[i]->checkContradiction())
-            insertedNodes[i]->markContradiction();
-    }
-}
 int achar_problema(string frase){
     /*  Problemas:
             1 = tautologia
@@ -90,9 +84,38 @@ char achar_operador(string expressao){
     }
     return operador;
 }
+void achar_subExpr(string expressao, string *substring1, string *substring2){
+    int i=0,par_esq = 0,par_dir = 0;
+    //caso atomico
+    if(expressao[i+1] != '('){
+        //se nao houver um ( apos o primeiro (, significa que e uma expressao atomica, ou seja, pegamos esse char para a substring1
+        *substring1 = expressao[i+1];
+        i+=5;
+    }
+    else{
+        //caso haja, fazemos um for indo do parentesis inicial ate o ultimo parentesis antes do operador da string
+        for(int j = 1;par_esq != par_dir && j < expressao.length(); j++){
+            //enquanto o numero de parentesis forem diferentes, copiar a string para a substring1
+            if(expressao[j]=='('){
+                par_esq ++;
+            }
+            else if(expressao[j] == ')'){
+                par_dir++;
+            }
+            *substring1.push_back(expressao[j]);
+        }
+        i+=4;
+    }
+    for(i;i<expressao[i].size()-1;i++){
+        //copiamos o resto da string ate o ultimo parentesis* (ignoramos o ultimo parentesis por ser da expressao completa)
+        *substring2.push_back(expressao[i]);
+    }
+}
+
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
+
     // ---- Arquivos ----
     ifstream entrada;
     ofstram saida;
@@ -111,7 +134,7 @@ int main() {
     entrada >> cursor;
     icursor = cursor;
 
-    for(int i = 0; i < n_enunciados;i++){
+    for(int i = 0;icursor > 0 && i < n_enunciados;i++){
         getline(cin,enunciados[i], '\n'); //recebe a linha ate o "\n"
         if(enunciados[i][0] == '('){
             //se o enunciado começa com (
@@ -121,18 +144,11 @@ int main() {
             }
         }
         else{
-            problema[i] = enunciados[i][0];
-            size_t posic = enunciados[i].find("{");
-            size_t lastposc = enunciados[i].rfind("}");
-            //problema[i]+= enunciados[i].substr(posic + 1);
-            proposicao[i].push_back(',');
-            for(int j = posic + 1; j <= lastposc - 1 ; j++){
-                proposicao[i].push_back(enunciados[i][j]);
-            }
+            proposicao[i] = enunciados[i][0];
         }
-
         problema[i] = achar_problema(enunciados[i]);
-            /*  Problemas:
+            /*  
+                Problemas:
             1 = tautologia
             2 = refutavel
             3 = insatisfativel
@@ -148,45 +164,104 @@ int main() {
             */
 
             if(problema[i] == 1){
+                //se for problema de tautologia
                 Node tableaux = Node(proposicao[i],false);
-                operador_principal = achar_operador(tableaux.expression);
-            }
+                arvore = tableaux;
+                }
             else if(problema[i] == 2){
+                //se for problema de refutabilidade
                 Node tableaux = Node(proposicao[i],false);
-                operador_principal = achar_operador(tableaux.expression);
+                arvore = tableaux;
                 }
 
             else if(problema[i] == 3){
+                //se for problema de insatisfatibilidade
                 Node tableaux = Node(proposicao[i],true);
-                operador_principal = achar_operador(tableaux.expression);
-            }
+                arvore = tableaux;
+                }
             else if(problema[i] == 4){
+                //se for problema de satisfatibilidade
                 Node tableaux = Node(proposicao[i],true);
-                operador_principal = achar_operador(tableaux.expression);
-            }
+                arvore = tableaux;
+                }
             else{
-                Node tableaux = Node(proposicao[i],true);
-            }
-    }
-    //criar o primeiro nó.
-    tableau.insertFront("P", 1);
-    tableau.insertFront("Q", 0);
+                //se for pboelam de consequencia logica
+                Node tableaux = Node(proposicao[i],false);
+                arvore = tableaux;
+                string conjunto_expr;
+                vector <Node *> inserir;
+                size_t posic = enunciados[i].find("{");
+                size_t lastposc = enunciados[i].rfind("}");
+                for(int ate_la = posic + 1; ate_la < lastposc;ate_la++){
+                    conjunto_expr.push_back(enunciados[ate_la]);
+                }
+                
+                while(conjunto_expr.length() > 0 && !arvore.isClosed()){
 
-    vector <Node*> appliableNodes = tableau.getAppliableNodes();
+                }
+            }
+
+            saida << "Problema #" << i + 1 << endl;
+
+            if(problema[i]==1) {
+                if(arvore.isClosed()){
+                   saida << "Nao, nao e satisfativel." << endl; 
+                } 
+                else{
+                    saida << "Sim, e satisfativel." << endl;
+                } 
+            }
+            else if(problema[i]==2) {
+                if(arvore.isClosed()){
+                    saida << "Nao, nao e refutavel." << endl;
+                } 
+                else{
+                    saida << "Sim, e refutavel." << endl;
+                }
+            }
+            else if(problema[i]==3) {
+                if(arvore.isClosed()){
+                    saida << "Sim, e tautologia." << endl;
+                } 
+                else{
+                    saida << "Nao, nao e tautologia." << endl;
+                } 
+            }
+            else if(problema[i]==4) {
+                if(arvore.isClosed()){
+                    saida << "Sim, e insatisfativel." << endl;
+                } 
+                else{
+                    saida << "Nao, nao e insatisfativel." << endl;
+                }
+            }
+            else {
+                if(arvore.isClosed()){
+                   saida << "Sim, e consequencia logica." << endl; 
+                } 
+                else{
+                    saida << "Nao, nao e consequencia logica." << endl;
+                }
+            }
+
+            icursor--;
+            if(icursor > 0) saida << endl;
+
+    }
+
+/*    vector <Node*> appliableNodes = tableau.getAppliableNodes();
     while(!tableau.isClosed() && !appliableNodes.empty()) {
-        /*for(Node* node : appliableNodes) {
-        }*/
+        //for(Node* node : appliableNodes) {
+        //}
         for(int i = 0; i < appliableNodes.size(); i++) {
             vector <Node*> insertedNodes = applyRule(appliableNodes[i]);
             checkContradictions(insertedNodes);
             appliableNodes = tableau.getAppliableNodes();
         }
     }
-
-    if(tableau.isClosed())
-        std::cout << "Tableau fechado." << std::endl;
-
-    tableau.printTree();
+*/
+    saida.close();
+    entrada.close();
 
     return 0;
 }
